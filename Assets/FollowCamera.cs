6255cf858963e399c980a8efeb0d31667521dd85
Brian Lai <brianlai30@outlook.com>
2019-08-24 22:52:21 -0700

    Initial commit

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = target.position;
+    }
+
+}
