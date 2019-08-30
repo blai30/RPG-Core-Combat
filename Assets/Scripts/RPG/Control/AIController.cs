75310697a629a7a53a7f8a087a896f5b3113caa0
Brian Lai <brianlai30@outlook.com>
2019-08-30 00:36:16 -0700

    Added patrol path and gizmos for the waypoints

--- Assets/Scripts/RPG/Control/AIController.cs
+++ Assets/Scripts/RPG/Control/AIController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control
@@ -10,17 +11,30 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 5f;
 
+        // Target player to chase
         private GameObject _targetPlayer;
 
+        /// <summary>
+        /// GameObject components
+        /// </summary>
         private Fighter _fighter;
         private Health _health;
+        private Mover _mover;
+
+        // Initial position
+        private Vector3 _guardPosition;
+        private float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start()
         {
             _targetPlayer = GameObject.FindWithTag("Player");
             _fighter = GetComponent<Fighter>();
             _health = GetComponent<Health>();
+            _mover = GetComponent<Mover>();
+
+            _guardPosition = transform.position;
         }
 
         private void Update()
@@ -33,12 +47,45 @@ namespace RPG.Control
 
             if (_fighter.GetIsInRange(_targetPlayer.transform, chaseDistance) && _fighter.CanAttack(_targetPlayer))
             {
-                _fighter.Attack(_targetPlayer);
+                // Attack target player when in range
+                AttackBehavior();
+                timeSinceLastSawPlayer = 0;
+            }
+            else if (timeSinceLastSawPlayer <= suspicionTime)
+            {
+                SuspicionBehavior();
             }
             else
             {
-                _fighter.Cancel();
+                // Go back to initial guarding position
+                GuardBehavior();
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Behavior for attacking the player
+        /// </summary>
+        private void AttackBehavior()
+        {
+            _fighter.Attack(_targetPlayer);
+        }
+
+        /// <summary>
+        /// Behavior for player exiting chase distance
+        /// </summary>
+        private void SuspicionBehavior()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        /// <summary>
+        /// Behavior for guarding their guard position
+        /// </summary>
+        private void GuardBehavior()
+        {
+            _mover.StartMoveAction(_guardPosition);
         }
 
         // Called by Unity
---
+++ Assets/Scripts/RPG/Control/PatrolPath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private const float WaypointGizmoRadius = 0.3f;
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypointPosition(i), WaypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypointPosition(i), GetWaypointPosition(j));
+            }
+        }
+
+        private int GetNextIndex(int index)
+        {
+            // Returns next index, 0 if next index is out of bounds
+            return (index + 1) % transform.childCount;
+        }
+
+        private Vector3 GetWaypointPosition(int index)
+        {
+            return transform.GetChild(index).position;
+        }
+    }
+}
